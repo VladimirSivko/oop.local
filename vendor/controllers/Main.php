6cb9575f69388135dd8be7e91ba80646567416e3
Vasyl <admin@gmail.com>
2018-12-14 17:01:42 +0200

    .htaccess index

--- vendor/controllers/Main.php
+++ vendor/controllers/Main.php
@@ -2,6 +2,18 @@
 
 namespace controllers;
 
-class Main {
+use core\controllers\Main as BaseController;
+use models\Articles;
+
+class Main extends BaseController{
     
+    public function __construct() {
+	parent::__construct();
+	$this->model=new Articles();
+    }
+    
+    public function index() {
+	$this->view->articles = $this->model->getAll();
+	$this->view->render();
+    }
 }
