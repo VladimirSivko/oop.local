6d1208624ebee8fc89f375fc3006bc1b92bbff27
Vasyl <admin@gmail.com>
2018-12-14 17:41:01 +0200

    change

--- vendor/core/controllers/Main.php
+++ vendor/core/controllers/Main.php
@@ -5,10 +5,12 @@ namespace core\controllers;
 use core\View;
 
 class Main {
+
     public $view;
     public $model;
+
     public function __construct() {
 	$this->view = new View();
     }
-    
+
 }
--- vendor/views/pages/index.php
+++ vendor/views/pages/index.php
@@ -1,7 +1,7 @@
 <div>
-    <?php foreach ($this->articles as $article):?>
-    <h2><?= $article['title']?></h2>
-    <p><?= $article['text']?></p>
-    <p><?= $article['date']?></p>
-    <?php endforeach;?>
+    <?php foreach ($this->articles as $article): ?>
+        <h2><?= $article['title'] ?></h2>
+        <p><?= $article['text'] ?></p>
+        <p><?= $article['date'] ?></p>
+    <?php endforeach; ?>
 </div>
