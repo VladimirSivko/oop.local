4cbd74500a175dae60f28af2c9ac2dd80e2913c6
Vasyl <admin@gmail.com>
2018-12-14 16:50:08 +0200

    base controllers/Main.php

--- vendor/core/controllers/Main.php
+++ vendor/core/controllers/Main.php
@@ -2,6 +2,13 @@
 
 namespace core\controllers;
 
+use core\View;
+
 class Main {
-    //put your code here
+    public $view;
+    public $model;
+    public function __construct() {
+	$this->view = new View();
+    }
+    
 }
