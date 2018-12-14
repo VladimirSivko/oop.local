5e1502691b5c1f38aa0b14726ba44e54af617add
Vasyl <admin@gmail.com>
2018-12-14 17:05:30 +0200

    Articles.php

--- vendor/models/Articles.php
+++ vendor/models/Articles.php
@@ -2,6 +2,31 @@
 
 namespace models;
 
-class Articles {
-    //put your code here
+use core\models\Main;
+
+class Articles  extends Main{
+    public function getAll(){
+	return array(
+	    array(
+		'title'=>'first',
+		'text'=>'many text',
+		'date'=>'14.12.18'
+	    ),
+	    array(
+		'title'=>'second',
+		'text'=>'many text',
+		'date'=>'14.12.18'
+	    ),
+	    array(
+		'title'=>'third',
+		'text'=>'many text',
+		'date'=>'14.12.18'
+	    ),
+	    array(
+		'title'=>'fourth',
+		'text'=>'many text',
+		'date'=>'15.12.18'
+	    )
+	);
+    }
 }
